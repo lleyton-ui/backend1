2e890cf1d99dd966b4ac82aeb050f9bd348b93c7
lleyton-ui <jltyabut.student@ua.edu.ph>
2025-12-09 09:12:32 +0800

    first commit

---
+++ events/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    location = models.CharField(max_length=100)
+    organizer = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
